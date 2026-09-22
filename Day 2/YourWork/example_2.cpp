67b7aeb6e1f88a6ea1a310b02f08b6a7f8231f78
tanim <tanim5129mh@gmail.com>
2026-09-22 23:20:31 +0600

    Add files via upload

---
+++ Day 2/YourWork/example_2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // You can use {} to instantly create and assign a pair
+    pair<string, int> student = {"Tanim", 189};
+    
+    cout << "Name: " << student.first << ", Roll: " << student.second << "\n";
+    
+    return 0;
+}
