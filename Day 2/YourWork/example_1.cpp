7b07b5aa5065555dcb823c627295e2bea47607d9
tanim <tanim5129mh@gmail.com>
2026-09-22 14:34:20 +0600

    Add files via upload

---
+++ Day 2/YourWork/example_1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    pair<int, int> p1;
+    p1.first = 10;
+    p1.second = 20;
+    
+    cout << "First value: " << p1.first << "\n";
+    cout << "Second value: " << p1.second << "\n";
+    
+    return 0;
+}
